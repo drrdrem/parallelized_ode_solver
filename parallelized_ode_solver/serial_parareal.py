4579fc30a98c24ef9e21373e615cfdac838e3d79
drrdrem <yin00425>
2022-04-25 12:47:34 -0500

    update serial parareal (still with bugs to be fixed)

--- parallelized_ode_solver/serial_parareal.py
+++ parallelized_ode_solver/serial_parareal.py
@@ -1,5 +1,5 @@
-'''
-Unfinished
+'''Seiral Version of ParaReal Algorithm
+
 '''
 from serial_erk import *
 
@@ -8,12 +8,13 @@ def parareal(csrRowPtr, csrColIdx, csrData, basis_fns, f_0, K, dt, coarse_alg, f
     fin_butcher_tableau, fin_dt = fine_alg[0], fine_alg[1]
     coars_butcher_tableau, coars_dt = coarse_alg[0], coarse_alg[1]
 
-    Nstep = (last_time)/dt
+    Nstep = int((last_time)/dt)
+    N_states = int(len(f_0))
 
     xs = [f_0]
     for _ in range(Nstep):
         x_t = erk(coars_butcher_tableau, csrRowPtr, csrColIdx, csrData, basis_fns, f_0, dt, coars_dt)
-        xs.append(xs)
+        xs.append(x_t)
 
     xks = [f_0]
     for k in range(K):
@@ -21,8 +22,36 @@ def parareal(csrRowPtr, csrColIdx, csrData, basis_fns, f_0, K, dt, coarse_alg, f
             h = erk(fin_butcher_tableau, csrRowPtr, csrColIdx, csrData, basis_fns, xs[t+1], dt, fin_dt)
             j = erk(coars_butcher_tableau, csrRowPtr, csrColIdx, csrData, basis_fns, xs[t], dt, coars_dt)
             g = erk(coars_butcher_tableau, csrRowPtr, csrColIdx, csrData, basis_fns, xks[-1], dt, coars_dt)
-
-            x_t = g + h - j 
+            
+            x_t = [g[i] + h[i] - j[i] for i in range(N_states)]
             xks.append(x_t)
         xs = xks
     return xs[-1]
+
+if __name__ == "__main__":
+    from time import time
+    from data_generator import *
+    c = [1/6, 1/3, 1/3, 1/6]
+    b = [0, 1/2, 1/2, 1]
+    A = [[0  ,   0, 0, 0],
+         [1/2,   0, 0, 0],
+         [0  , 1/2, 0, 0],
+         [0  ,   0, 1, 0]]
+    rk4 = butcher_tableau(c, b, A)
+
+    # First example linear
+    def linear_basis(t, states):
+        return states
+
+    dim, csrRowPtr, csrColIdx, csrData = generateCSRMatrix(15)
+
+    coarse_alg = (rk4, 0.05) 
+    fine_alg = (rk4, 0.01) 
+    K = 10
+    f_0 = [1. for _ in range(dim)]
+    st = time()
+    serial_res = parareal(csrRowPtr, csrColIdx, csrData, linear_basis, f_0, K, 0.1, coarse_alg, fine_alg)
+
+    ed = time()
+    print(serial_res)
+    print("Serial Time elapse: {}".format(ed-st))
