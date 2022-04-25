52f887fbc80d8c348b4cb73c3efcf79942914c96
drrdrem <yin00425>
2022-04-25 11:38:34 -0500

    update serial parareal

--- parallelized_ode_solver/serial_parareal.py
+++ parallelized_ode_solver/serial_parareal.py
@@ -1,32 +1,28 @@
 '''
 Unfinished
 '''
-
-import numpy as np
-from functools import partial as fp
-
 from serial_erk import *
 
-def parareal(coarse_alg, fine_alg, p, K, d_t, initial_value, last_time=1.0):
-    steps = int((last_time)/d_t)
-    steps -= steps % p
+def parareal(csrRowPtr, csrColIdx, csrData, basis_fns, f_0, K, dt, coarse_alg, fine_alg, last_time=1.0):
 
-    dt_coarse = (last_time)/p
+    fin_butcher_tableau, fin_dt = fine_alg[0], fine_alg[1]
+    coars_butcher_tableau, coars_dt = coarse_alg[0], coarse_alg[1]
 
-    vec = coarse_func(t_start, t_stop, u_0, delta_t_coarse)
-    last_coarse = coarse_func(t_start, t_stop, u_0, delta_t_coarse)
+    Nstep = (last_time)/dt
+
+    xs = [f_0]
+    for _ in range(Nstep):
+        x_t = erk(coars_butcher_tableau, csrRowPtr, csrColIdx, csrData, basis_fns, f_0, dt, coars_dt)
+        xs.append(xs)
+
+    xks = [f_0]
     for k in range(K):
-        updated_coarse = np.zeros(p+1)
-        tmp = np.zeros(p+1)
-        tmp[0] = vec[0] # initial value
-        for p, time in zip(range(p), time_intervals):
-            start, stop = time
-            fine_calc = fine_func(start, stop, vec[p],delta_t)
-            delta_q = fine_calc[-1] - last_coarse[p+1]
-            new_q = coarse_func(start, stop, tmp[p], delta_t_coarse)
-            updated_coarse[p+1] = new_q[-1]
-            tmp[p+1] = (updated_coarse[p+1] - last_coarse[p+1]) + fine_calc[-1]
+        for t in range(Nstep):
+            h = erk(fin_butcher_tableau, csrRowPtr, csrColIdx, csrData, basis_fns, xs[t+1], dt, fin_dt)
+            j = erk(coars_butcher_tableau, csrRowPtr, csrColIdx, csrData, basis_fns, xs[t], dt, coars_dt)
+            g = erk(coars_butcher_tableau, csrRowPtr, csrColIdx, csrData, basis_fns, xks[-1], dt, coars_dt)
 
-        vec = tmp
-        last_coarse = updated_coarse
-    return vec
+            x_t = g + h - j 
+            xks.append(x_t)
+        xs = xks
+    return xs[-1]
